423a0738d90ac32fde7ff79e38dbdf5ea36ddda5
Marco de Sousa <marcosousa@id.uff.br>
2019-05-20 21:33:35 -0300

    adicionado tela inicial, tela de loading, tela de info, tela de regras

---
+++ RummikUFF/Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(LoadNewScene());
+
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        //atrasar em 3 segundos a chamada da proxima tela para agente poder ver a tela de loading
+        yield return null;
+        AsyncOperation ao = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("TeladoJogo");
+        if (ao.isDone)
+        {
+            //yield return new WaitForSeconds(3);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("TeladoJogo");
+
+        }
+
+    }
+}
---
+++ RummikUFF/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInicial : MonoBehaviour {
+
+    public void ChamaTelaInfo()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TelaInfo");
+
+    }
+    public void ChamaTelaRegras()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TelaDeRegras");
+
+    }
+    public void ChamaTelaDeLoading()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TelaLoading");
+
+    }
+    public void ChamaTelaInicial()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TelaInicial");
+
+    }
+    public void ChamaTelaJogo()
+    {
+        System.Threading.Thread.Sleep(3000);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TelaInicial");
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+}
